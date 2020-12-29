928699f36c178e2fe5b4d069970c9c9fe30171ad
jvratanar@email.com <jvratanar@email.com>
2020-12-29 19:18:44 +0100

    Many to Many relationship.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -15,8 +15,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // TODO add
-    // private Difficulty difficulty
 
     // Recipe is owner - cascade, Ingredient entity must have property recipe
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
@@ -25,10 +23,23 @@ public class Recipe {
     @Lob // Large OBject field - blob
     private Byte[] image;
 
+    // It is desirable to set the enum values to String. The value
+    // is then stored in the db. If we insert some new enum between
+    // the two, the enums after if will not have the same values but
+    // incremented by one. This leaves us with wrong values stored in
+    // the db for enums as there are old ones, not the one incremented.
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     // cascade definition specifies the Recipe to own the Notes e.g. if Recipe is
     // deleted then delete the Notes entity as well.
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -102,11 +113,35 @@ public class Recipe {
         this.image = image;
     }
 
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
     public Notes getNotes() {
         return notes;
     }
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
