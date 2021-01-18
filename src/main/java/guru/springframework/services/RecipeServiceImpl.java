e75cb90d684e1988cf4e01e226d79cd8b9ddda5e
jvratanar@email.com <jvratanar@email.com>
2021-01-18 15:44:30 +0100

    193. Display a Recipe by ID.

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -25,5 +26,16 @@ public class RecipeServiceImpl implements RecipeService {
         this.recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
 
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(long id) {
+        Optional<Recipe> recipe = this.recipeRepository.findById(id);
+
+        if (!recipe.isPresent()) {
+            throw new RuntimeException("Recipe Not Found!");
+        }
+
+        return recipe.get();
     }
 }
